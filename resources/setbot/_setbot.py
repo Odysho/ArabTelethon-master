b74e1c9cf9d557d182e786addad3cf26d9ab999e
odisho1997 <86355012+odisho1997@users.noreply.github.com>
2021-08-09 23:20:53 +0300

    Add files via upload

---
+++ resources/setbot/_setbot.py
@@ -0,0 +1,48 @@
+import asyncio
+import telethon.utils
+from userbot import LOGS, bot
+
+from resources.Formation.assistant import SetMsg, SteDec, SetAbt
+from userbot.tosh import TOSHA, TBOT
+
+async def setbot():
+        try:
+            entit = await bot.get_entity(TBOT)
+            if entit.photo == None:
+               LOGS.info(SetMsg[0])
+               UL = TBOT
+               if bot.me.username == None:
+                   first_name = bot.me.first_name
+               else:
+                   first_name = f"@{bot.me.username}"
+               await bot.send_message(TOSHA, SetMsg[0])
+               await asyncio.sleep(1)
+               await bot.send_message("@botfather", "/cancel")
+               await asyncio.sleep(1)
+               await bot.send_message("@botfather", "/start")
+               await asyncio.sleep(1)
+               await bot.send_message("@botfather", "/setuserpic")
+               await asyncio.sleep(1)
+               await bot.send_message("@botfather", UL)
+               await asyncio.sleep(1)
+               await bot.send_file("@botfather", "userbot/extras/ex_7.jpeg")
+               await asyncio.sleep(2)
+               await bot.send_message("@botfather", "/setabouttext")
+               await asyncio.sleep(1)
+               await bot.send_message("@botfather", UL)
+               await asyncio.sleep(1)
+               await bot.send_message("@botfather", SetAbt.format(first_name))
+               await asyncio.sleep(2)
+               await bot.send_message("@botfather", "/setdescription")
+               await asyncio.sleep(1)
+               await bot.send_message("@botfather", UL)
+               await asyncio.sleep(1)
+               await bot.send_message("@botfather", SetDec.format(first_name))
+               await asyncio.sleep(2)
+               await bot.send_message("@botfather", "/start")
+               await asyncio.sleep(1)
+               await bot.send_message(TOSHA, SetMsg[1])
+               LOGS.info(SetMsg[2])
+        except Exception as e:
+            LOGS.info(str(e))
+
