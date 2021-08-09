2729014cef0e0612f2958595971f908c4e24689e
odisho1997 <86355012+odisho1997@users.noreply.github.com>
2021-08-09 23:35:21 +0300

    Add files via upload

---
+++ userbot/__main__.py
@@ -0,0 +1,77 @@
+import sys
+
+import userbot
+from userbot import BOTLOG_CHATID, HEROKU_APP, PM_LOGGER_GROUP_ID
+
+from .Config import Config
+from .core.logger import logging
+from .core.session import iqthon
+from .utils import (
+    add_bot_to_logger_group,
+    ipchange,
+    load_plugins,
+    setup_bot,
+    startupmessage,
+    verifyLoggerGroup,
+)
+
+LOGS = logging.getLogger("تليثون العرب")
+
+print(userbot.__copyright__)
+print("المرخصة بموجب شروط " + userbot.__license__)
+
+cmdhr = Config.COMMAND_HAND_LER
+
+try:
+    LOGS.info("بدء تنزيل تليثون العرب")
+    iqthon.loop.run_until_complete(setup_bot())
+    LOGS.info("بدء تشغيل البوت")
+except Exception as e:
+    LOGS.error(f"{str(e)}")
+    sys.exit()
+
+
+class CatCheck:
+    def __init__(self):
+        self.sucess = True
+
+
+Catcheck = CatCheck()
+
+
+async def startup_process():
+    check = await ipchange()
+    if check is not None:
+        Catcheck.sucess = False
+        return
+    await verifyLoggerGroup()
+    await load_plugins("plugins")
+    await load_plugins("assistant")
+    print("❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖\n")
+    print("حسنا لقد تم تنصيب تليثون العرب بدون مشاكل")
+    print(
+        f"⌔︙ا  تهانينا ، اكتب الآن -  .السورس  - لروئيه المعلومات\
+        \nقم بتوجه الى القناه لمعرفه الاوامر وباقي معلومات https://t.me/iqthon"
+    )
+    print("❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖")
+    await verifyLoggerGroup()
+    await add_bot_to_logger_group(BOTLOG_CHATID)
+    if PM_LOGGER_GROUP_ID != -100:
+        await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
+    await startupmessage()
+    Catcheck.sucess = True
+    return
+
+
+iqthon.loop.run_until_complete(startup_process())
+
+if len(sys.argv) not in (1, 3, 4):
+    iqthon.disconnect()
+elif not Catcheck.sucess:
+    if HEROKU_APP is not None:
+        HEROKU_APP.restart()
+else:
+    try:
+        iqthon.run_until_disconnected()
+    except ConnectionError:
+        pass
