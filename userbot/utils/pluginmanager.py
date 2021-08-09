0cf5caa960a356bd2dd4a7c9f4671304fd1e7309
odisho1997 <86355012+odisho1997@users.noreply.github.com>
2021-08-10 02:55:55 +0300

    Update pluginmanager.py

--- userbot/utils/pluginmanager.py
+++ userbot/utils/pluginmanager.py
@@ -26,7 +26,7 @@ def load_module(shortname, plugin_path=None):
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(mod)
-        LOGS.info("⌔︙ تم بنجاح تحميل ملف " + shortname)
+        LOGS.info( shortname + " ├ تم بنجاح تحميل ملف \n")
     else:
         if plugin_path is None:
             path = Path(f"userbot/plugins/{shortname}.py")
@@ -58,7 +58,7 @@ def load_module(shortname, plugin_path=None):
         spec.loader.exec_module(mod)
         # for imports
         sys.modules["userbot.plugins." + shortname] = mod
-        LOGS.info("⌔︙ تم بنجاح تحميل ملف " + shortname)
+        LOGS.info( shortname + " ├ تم بنجاح تحميل ملف \n")
 
 
 def remove_plugin(shortname):
